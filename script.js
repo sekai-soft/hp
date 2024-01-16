169747c9fc7cc84f7b614df1c8dbd5285ec0638e
KTachibanaM <whj19931115@gmail.com>
2024-01-15 18:00:18 -0800

    configurable scroll duration

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+const sections = $('.fullscreen');
+const scrollDuration = 200;
+
 let currentSection = 0;
-let sections = $('.fullscreen');
-$(window).on('wheel', function(e) {
-    if (e.originalEvent.deltaY > 0) {
+
+$(window).on('wheel', (e) => {
+    navigateSections(e.originalEvent.deltaY); 
+});
+
+const navigateSections = (deltaY) => {
+    if (deltaY > 0) {
         currentSection++;
     } else {
         currentSection--;
@@ -9,5 +16,5 @@ $(window).on('wheel', function(e) {
     currentSection = Math.min(Math.max(currentSection, 0), sections.length - 1);
     $('html, body').animate({
         scrollTop: $(sections[currentSection]).offset().top
-    });
-});
+    }, scrollDuration);
+}
